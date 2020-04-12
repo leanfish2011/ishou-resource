e62b3d0388e7bea1d2fa8795c9f7d8f08638d5a7
yangtzeyufei <yangtze_yufei@163.com>
2020-04-12 18:16:44 +0800

    镜像名称集中

--- docker/docker_start.sh
+++ docker/docker_start.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+# 所有镜像
+image_portainer="portainer:20200329_212642"
+image_ishou_mariadb_infra="mariadb:20200329_204923"
+image_ishou_redis_infra="redis:20200329_204223"
+image_ishou_nginx_infra="ishou_nginx_infra:v1.0_dev_20200412_170628_1e1bfdd"
+image_ishou_mariadb_init="ishou_mariadb_init:v1.0_dev_20200411_154744_981cbab"
+image_ishou_eureka_service="eureka-server:v1.0_dev_20200406_183306_19da952"
+image_ishou_auth_service="auth:v1.0_dev_20200412_172850_ac7eb73"
+image_ishou_site_service="ishou-service-site:v1.0_dev_20200411_173205_a4183bd"
+image_ishou_web="ishou-web:v1.0_dev_20200412_174519_5838952"
+
+
 read -p "确定重新部署服务 (y/n)?" CONT
 if [ "$CONT" = "y" ]; then
   echo "重新部署服务，请确认旧的服务已经关闭！";
@@ -21,7 +33,7 @@ sudo docker run -d --net=host \
   --restart=always \
   -v /var/run/docker.sock:/var/run/docker.sock \
   --name portainer \
-  portainer:20200329_212642
+  $image_portainer
 
 echo "1、mariadb 基础服务镜像启动，端口：3306"
 sudo docker run -d --net=host \
@@ -29,14 +41,14 @@ sudo docker run -d --net=host \
   -v $data_path"/mariadb":/var/lib/mysql \
   --name ishou_mariadb_infra \
   -e MYSQL_ROOT_PASSWORD=$global_password \
-  mariadb:20200329_204923
+  $image_ishou_mariadb_infra
 
 echo "2、redis 基础服务镜像启动，端口：6379"
 sudo docker run -d --net=host \
   --restart=always \
   -v $data_path"/redis":/data \
   --name ishou_redis_infra \
-  redis:20200329_204223 \
+  $image_ishou_redis_infra \
   redis-server --appendonly yes --requirepass $global_password
 
 echo "3、nginx 基础服务镜像启动，端口：80"
@@ -45,7 +57,7 @@ sudo docker run -d --net=host \
   -v $data_path"/nginx/web":/usr/share/nginx/html \
   -v $data_path"/nginx/log":/var/log/nginx \
   --name ishou_nginx_infra \
-  nginx:20200329_204023
+  $image_ishou_nginx_infra
 
 echo "部署中……"
 sleep 1m
@@ -55,13 +67,13 @@ sudo docker run -d --net=host \
   -v $data_path"/init/mariadb/backup":/home/mysql/backup \
   --name ishou_mariadb_init \
   -e MYSQL_ROOT_PASSWORD=$global_password \
-  ishou_mariadb_init:v1.0_dev_20200411_154744_981cbab
+  $image_ishou_mariadb_init
 
 echo "5、eureka 服务启动，端口：9999"
 sudo docker run -d --net=host \
   --restart always \
   --name ishou_eureka_service \
-  eureka-server:v1.0_dev_20200406_183306_19da952
+  $image_ishou_eureka_service
 
 echo "部署中……"
 sleep 1m
@@ -72,20 +84,20 @@ sudo docker run -d --net=host \
   --name ishou_auth_service \
   -e MYSQL_ROOT_PASSWORD=$global_password \
   -e REDIS_PASSWORD=$global_password \
-  auth:v1.0_dev_20200411_154350_39c0f8f
+  $image_ishou_auth_service
 
 echo "7、site 服务启动，端口：9092"
 sudo docker run -d --net=host \
   --restart always \
   --name ishou_site_service \
   -e MYSQL_ROOT_PASSWORD=$global_password \
-  ishou-service-site:v1.0_dev_20200411_173205_a4183bd
+  $image_ishou_site_service
 
 echo "8、前端镜像启动"
 sudo docker run -d --net=host \
   --name ishou_web \
   -v $data_path"/nginx/web":/opt/project/web \
-  ishou-web:v1.0_dev_20200411_154858_b89b1c8
+  $image_ishou_web
 
 echo "部署中……"
 sleep 1m
